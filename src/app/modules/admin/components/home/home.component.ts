25df0a68a65e00d1e09c5335a028288a29b75cd9
Shorif Ahmed <sashipon001@gmail.com>
2022-03-05 18:06:19 +0600

    oninit ondestroy

--- src/app/modules/admin/components/home/home.component.ts
+++ src/app/modules/admin/components/home/home.component.ts
@@ -1,20 +1,26 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnChanges {
+export class HomeComponent implements OnInit, OnDestroy, OnChanges {
 
   constructor() {
     console.log("parent contructor view");
   }
 
   message = "hello world"
+  counter = 0;
 
   ngOnInit(): void {
     console.log("parent OnInit view");
+
+    setInterval(()=>{
+      this.counter = this.counter + 1;
+      console.log(this.counter);
+    }, 1000)
   }
 
   // receiveMessage($event:any){
@@ -22,12 +28,17 @@ export class HomeComponent implements OnInit, OnChanges {
   // }
 
 
-  isChild = true;
+  isChild = false;
   
   channelName = "";
   @Input()
   toggleChild(){
     this.isChild = !this.isChild;
+  }
+
+
+  ngOnDestroy(): void {
+    console.log("parent OnDestroy view");
   }
 
  ngOnChanges(): void {
