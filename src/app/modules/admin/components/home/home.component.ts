3b42585024e44865e9cc5fc566f6aff40299331b
Shorif Ahmed <sashipon001@gmail.com>
2022-03-20 17:25:00 +0600

    update

--- src/app/modules/admin/components/home/home.component.ts
+++ src/app/modules/admin/components/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   message = "hello world"
-  counter = 0;
+  // counter = 0;
   // interval: any;
 
   ngOnInit(): void {
@@ -46,6 +46,155 @@ export class HomeComponent implements OnInit, OnDestroy, OnChanges {
  ngOnChanges(): void {
   console.log("parent OnChanges view");
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+// interpolation start 
+
+appName = "This is app";
+name="shorif"
+
+getName(){
+  return "This is function"
+}
+
+obj={
+  name: "This is object name",
+  age: 20
+}
+
+arr=['this is array name', 'this is array name1', 'this is array name2']
+
+siteUrl = window.location.href;
+
+// interpolation end 
+
+
+
+
+
+
+
+// function on button click
+getName1(paramiterPass:any){
+  alert(paramiterPass)
+}
+
+// event
+
+getVal(){
+  console.log("shorif");
+}
+
+getData(val:string){
+  console.log(val);
+}
+
+
+
+
+
+
+
+
+// input field value
+
+title = 'shorif'
+
+displayVal="";
+getValue(val:string){
+  console.log(val);
+  this.displayVal=val
+}
+// click value
+displayVal1="";
+getValue1(val:string){
+  console.log(val);
+  this.displayVal1=val
+}
+
+
+
+// counter example
+
+
+count=0;
+counter(type:string){
+  // this.count1++
+  type==='plus'?this.count++:this.count--
+}
+
+
+
+
+
+
+// property binding
+name1:string="shorif";
+disable=true;
+
+// if else 
+
+show=false
+
+
+//ngFor
+
+// users=['shorif', 'rohim', 'korim'];
+
+
+// users=[
+//   {name: "shorif", email: "a@B.com", phone: "010101"},
+//   {name: "rohim", email: "r@r.com", phone: "010102"}
+// ];
+
+
+//nested loop
+
+nestedLoop = [
+  {name: "a",phone:"1111", email:"a@a.com", accounts:['facebook', 'gmail', 'yahoomail']},
+  {name: "b",phone:"2222", email:"b@b.com", accounts:['youtube', 'instagram', 'ymail']},
+  {name: "c",phone:"3333", email:"c@c.com", accounts:['linkedin', 'gmail', 'aolmail']}
+]
+
+
+
+
+
+
+getname11:any= '';
+getName11(data:any){
+  console.log(data);
+  this.getname11 = data;
+}
+
+
+
+userData = [];
+
+inputFieldData(data:any){
+  console.log(data);
+  this.userData = data;
+}
+
+
+
+
+
+
+
 
 
 }
